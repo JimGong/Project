3f29e8822eeb221e2fae399534e31f82356ff163
JimGong <jgong7@usfca.edu>
2015-09-18 17:41:21 -0700

    ..

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 /**
@@ -9,100 +14,115 @@ import java.util.Arrays;
  */
 public class Driver {
 
-    /**
-     * Flag used to indicate the following value is an input directory of text
-     * files to use when building the inverted index.
-     * 
-     * @see "Projects 1 to 5"
-     */
-    public static final String INPUT_FLAG = "-input";
+	/**
+	 * Flag used to indicate the following value is an input directory of text
+	 * files to use when building the inverted index.
+	 *
+	 * @see "Projects 1 to 5"
+	 */
+	public static final String INPUT_FLAG = "-input";
 
-    /**
-     * Flag used to indicate the following value is the path to use when
-     * outputting the inverted index to a JSON file. If no value is provided,
-     * then {@link #INDEX_DEFAULT} should be used. If this flag is not provided,
-     * then the inverted index should not be output to a file.
-     * 
-     * @see "Projects 1 to 5"
-     */
-    public static final String INDEX_FLAG = "-index";
+	/**
+	 * Flag used to indicate the following value is the path to use when
+	 * outputting the inverted index to a JSON file. If no value is provided,
+	 * then {@link #INDEX_DEFAULT} should be used. If this flag is not provided,
+	 * then the inverted index should not be output to a file.
+	 *
+	 * @see "Projects 1 to 5"
+	 */
+	public static final String INDEX_FLAG = "-index";
 
-    /**
-     * Flag used to indicate the following value is a text file of search
-     * queries.
-     * 
-     * @see "Projects 2 to 5"
-     */
-    public static final String QUERIES_FLAG = "-query";
+	/**
+	 * Flag used to indicate the following value is a text file of search
+	 * queries.
+	 *
+	 * @see "Projects 2 to 5"
+	 */
+	public static final String QUERIES_FLAG = "-query";
 
-    /**
-     * Flag used to indicate the following value is the path to use when
-     * outputting the search results to a JSON file. If no value is provided,
-     * then {@link #RESULTS_DEFAULT} should be used. If this flag is not
-     * provided, then the search results should not be output to a file.
-     * 
-     * @see "Projects 2 to 5"
-     */
-    public static final String RESULTS_FLAG = "-results";
+	/**
+	 * Flag used to indicate the following value is the path to use when
+	 * outputting the search results to a JSON file. If no value is provided,
+	 * then {@link #RESULTS_DEFAULT} should be used. If this flag is not
+	 * provided, then the search results should not be output to a file.
+	 *
+	 * @see "Projects 2 to 5"
+	 */
+	public static final String RESULTS_FLAG = "-results";
 
-    /**
-     * Flag used to indicate the following value is the number of threads to use
-     * when configuring multithreading. If no value is provided, then
-     * {@link #THREAD_DEFAULT} should be used. If this flag is not provided,
-     * then multithreading should NOT be used.
-     * 
-     * @see "Projects 3 to 5"
-     */
-    public static final String THREAD_FLAG = "-threads";
+	/**
+	 * Flag used to indicate the following value is the number of threads to use
+	 * when configuring multithreading. If no value is provided, then
+	 * {@link #THREAD_DEFAULT} should be used. If this flag is not provided,
+	 * then multithreading should NOT be used.
+	 *
+	 * @see "Projects 3 to 5"
+	 */
+	public static final String THREAD_FLAG = "-threads";
 
-    /**
-     * Flag used to indicate the following value is the seed URL to use when
-     * building the inverted index.
-     * 
-     * @see "Projects 4 to 5"
-     */
-    public static final String SEED_FLAG = "-seed";
+	/**
+	 * Flag used to indicate the following value is the seed URL to use when
+	 * building the inverted index.
+	 *
+	 * @see "Projects 4 to 5"
+	 */
+	public static final String SEED_FLAG = "-seed";
 
-    /**
-     * Flag used to indicate the following value is the port number to use when
-     * starting a web server. If no value is provided, then
-     * {@link #PORT_DEFAULT} should be used. If this flag is not provided, then
-     * a web server should not be started.
-     */
-    public static final String PORT_FLAG = "-port";
+	/**
+	 * Flag used to indicate the following value is the port number to use when
+	 * starting a web server. If no value is provided, then
+	 * {@link #PORT_DEFAULT} should be used. If this flag is not provided, then
+	 * a web server should not be started.
+	 */
+	public static final String PORT_FLAG = "-port";
 
-    /**
-     * Default to use when the value for the {@link #INDEX_FLAG} is missing.
-     */
-    public static final String INDEX_DEFAULT = "index.json";
+	/**
+	 * Default to use when the value for the {@link #INDEX_FLAG} is missing.
+	 */
+	public static final String INDEX_DEFAULT = "index.json";
 
-    /**
-     * Default to use when the value for the {@link #RESULTS_FLAG} is missing.
-     */
-    public static final String RESULTS_DEFAULT = "results.json";
+	/**
+	 * Default to use when the value for the {@link #RESULTS_FLAG} is missing.
+	 */
+	public static final String RESULTS_DEFAULT = "results.json";
 
-    /**
-     * Default to use when the value for the {@link #THREAD_FLAG} is missing.
-     */
-    public static final int THREAD_DEFAULT = 5;
-    
-    /**
-     * Default to use when the value for the {@link #PORT_FLAG} is missing.
-     */
-    public static final int PORT_DEFAULT = 8080;
+	/**
+	 * Default to use when the value for the {@link #THREAD_FLAG} is missing.
+	 */
+	public static final int THREAD_DEFAULT = 5;
 
-    /**
-     * Parses the provided arguments and, if appropriate, will build an inverted
-     * index from a directory or seed URL, process search queries, configure
-     * multithreading, and launch a web server.
-     * 
-     * @param args
-     *            set of flag and value pairs
-     */
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
+	/**
+	 * Default to use when the value for the {@link #PORT_FLAG} is missing.
+	 */
+	public static final int PORT_DEFAULT = 8080;
 
-        // TODO Fill this in and add additional methods and classes as
-        // necessary.
-    }
+	/**
+	 * Parses the provided arguments and, if appropriate, will build an inverted
+	 * index from a directory or seed URL, process search queries, configure
+	 * multithreading, and launch a web server.
+	 *
+	 * @param args
+	 *            set of flag and value pairs
+	 */
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(args));
+
+		InvertedIndex i = new InvertedIndex();
+		i.dirTraverse(new File("/Users/JiaMinGong/Desktop/TestFolder"));
+		String name = "output.json";
+		Path outputPath = Paths.get(".", "output", name);
+
+		try {
+
+			Files.createDirectories(outputPath.getParent());
+
+			Files.deleteIfExists(outputPath);
+
+		} catch (IOException e) {
+			System.out.println("screw you");
+			e.printStackTrace();
+		}
+
+		i.printWordMap(outputPath);
+	}
 }
