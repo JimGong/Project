29e98981c9bfdee6517bb218aed5b606bd4c2769
JimGong <jgong7@usfca.edu>
2015-11-18 19:56:26 -0800

    passed all test for project 3

--- src/WorkQueue.java
+++ src/WorkQueue.java
@@ -177,7 +177,7 @@ public class WorkQueue {
 					r.run();
 				} catch (RuntimeException ex) {
 					// catch runtime exceptions to avoid leaking threads
-					ex.printStackTrace();
+					// ex.printStackTrace();
 					System.err.println("Warning: Work queue encountered an "
 							+ "exception while running.");
 					// }
