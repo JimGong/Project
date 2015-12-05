8a37a7aebb2abe78a4da9b63ed238309609a6da8
Sophie Engle <sjengle@cs.usfca.edu>
2015-12-04 17:16:39 -0800

    Update WebCrawler.java

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -9,7 +9,8 @@ import org.apache.logging.log4j.Logger;
 public class WebCrawler {
 
 	// private LinkedHashSet<String> links;
-	private ArrayList<String> links;
+	private ArrayList<String> links; // TODO Should be able to use a hashset!
+	
 	private ThreadSafeInvertedIndex index;
 	private final WorkQueue minions;
 	private static final Logger logger = LogManager.getLogger();
@@ -24,10 +25,24 @@ public class WebCrawler {
 		this.index = index;
 	}
 
+	/* TODO Different approach
+	public void traverse(String url) {
+		lock.lockReadWrite();
+		
+		if (set.size() < 50 && !set.contains(url)) {
+			set.add(url);
+			minions.execute(new worker...)
+		}
+		
+		lock.unlock...
+	}
+	*/
+
 	public void traverse(String url) throws MalformedURLException {
 		logger.debug("adding {} into links", url);
-		links.add(url);
+		links.add(url); // TODO Unprotected access, which only works because you are basically single-threading
 
+		// TODO Super inefficient, basically single-threading this
 		for (int i = 0; (i < MAX_CAPACITY) && (i < links.size()); i++) {
 			String link = links.get(i);
 
@@ -59,6 +74,15 @@ public class WebCrawler {
 				 * ArrayList if links size smaller than 50
 				 */
 				ArrayList<String> innerURLs = LinkParser.listLinks(html);
+				
+				/* TODO
+				Lock once
+				
+				Loop through links... for all unique links create a new worker
+
+				Unlock
+				*/
+				
 				// logger.debug("find {} innerURL in link {}", innerURLs.size(),
 				// link);
 				for (int i = 0; (i < innerURLs.size())
@@ -82,14 +106,20 @@ public class WebCrawler {
 				// logger.debug("html splitted into words");
 				int position = 1;
 
+				// TODO Add to local first so you don't cause constant locking
+
 				/* parse words into inverted index one at a time */
 				for (String word : words) {
 					index.add(word, link, position);
 					position++;
 				}
+				
+				// TODO Addall to main index
+				
 				logger.debug("passed all words for this {} into index", link);
 
 			} catch (IOException e) {
+				// TODO Stack trace!
 				e.printStackTrace();
 			}
 			logger.debug("######## Minion finished {}", link);
